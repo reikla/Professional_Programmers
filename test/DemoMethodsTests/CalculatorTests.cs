882295ac1f8b38458aa12d892d269ebe02f61c49
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-11-24 14:36:24 +0100

    Arrange/Act/Assert PAttern

--- test/DemoMethodsTests/CalculatorTests.cs
+++ test/DemoMethodsTests/CalculatorTests.cs
@@ -7,17 +7,23 @@ public class CalculatorTests
     [Fact]
     public void Add_PositiveNumbers_HappyPath()
     {
+        //Arrange
         var c = new Calculator();
 
+        //Act
         var result = c.Add(4, 5);
 
+        //Assert
         Assert.Equal(9, result);
     }
 
     [Fact]
     public void Subtract_PositiveNumbers_HappyPath()
     {
+        // Arrange, Act
         var result = Calculator.Subtract(3, 1);
+        
+        //Assert
         Assert.Equal(2, result);
     }
 }
