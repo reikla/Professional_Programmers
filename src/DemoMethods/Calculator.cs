0bacbced74e2c8620671d0347b610c61e4d382bc
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-11-24 14:33:54 +0100

    Fixe Codestyle/add assignment

--- src/DemoMethods/Calculator.cs
+++ src/DemoMethods/Calculator.cs
@@ -1,7 +1,14 @@
 namespace DemoMethods;
 
-public class Calculator { public int Add(int a, int b) { return a + 
-                                                                b; } public static int Subtract(int a, int b) {
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
         return a - b;
     }
 }
